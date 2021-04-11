9b0a2e25886f7829f36680e2a42673e4276d603d
rkorkin <rkorkin@slb.com>
2021-04-12 00:26:18 +0700

    names changed

--- tests/test_monster.py
+++ tests/test_monster.py
@@ -9,25 +9,25 @@ from tests.test_modules import test_1
 def test_callable():
     assert isinstance(test_1, types.ModuleType)
     isok = methods_importer(
-        method_name='callable_method',
+        method_name='test_callable',
         modules_test=(test_1, 
-    assert isok == [(test_1, .callable_method]
+    assert isok == [test_1.test_callable]
 
 def test_non_callable_const():
     assert isinstance(test_1, types.ModuleType)
-    isok = methods_importer(method_name='const', modules=[test1])
+    isok = methods_importer(method_name='test_non_callable_const', modules=[test1])
     assert isok == []
 
 def test_non_callable_method():
     assert isinstance(test_1, types.ModuleType)
-    isok = methods_importer(method_name='non_collable_method', modules=[test1])
+    isok = methods_importer(method_name='test_non_callable_method', modules=[test1])
     assert isok == []
 
 def test_incorrect_type():
     with pytest.raises(TypeError):
-        methods_importer(method_name='callable_test', modules=[test1])
+        methods_importer(method_name='test_callable', modules=[test1])
 
-def test_str_modul():
+def test_str_module():
     isok = methods_importer(method_name='sum', modules=['numpy'])
     assert isok == [numpy.sum]
 
@@ -40,5 +40,5 @@ def two_modules_non_collable_test():
     assert isok == []
 
 def test_methods_importer_other_module():
-    isok = methods_importer(method_name='my_sum', modules=[test1, 'other'])
-    assert isok == [test1.my_sum]
+    isok = methods_importer(method_name='my_sum', modules=[test_1, 'other'])
+    assert isok == [test_1.my_sum]
